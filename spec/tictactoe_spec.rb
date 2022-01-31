fb34ff1a0ea60f2614b06f0150376d89df477cb2
naivedjack <naivedjack@protonmail.com>
2022-01-31 08:50:06 +0100

    added test expectation comments

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -26,10 +26,15 @@ describe Game do
   subject(:game) { described_class.new }
 
   describe '#turn' do
+    # a turn happens
+    # turn counter goes up
     xit 'plays one turn' do
       expect(game).to receive(:turn_play)
     end
 
+    # turn counter gets to 10
+    # turn counter does not go up
+    # game ends
     xit 'stops the game at turn 10' do
       game.instance_variable_set(:@turn_counter, 10)
       expect(game.game).to be false
@@ -39,6 +44,9 @@ describe Game do
   describe '#check_win' do
     let(:winning_grid) { instance_double(Grid) }
 
+    # mock grid becomes Game @grid
+    # check win on the mock grid
+    # game over is called
     before do
       winning_grid.instance_variable_set(:@cells, ['X', 'X', 'X', 'O', 'O', ' ', ' ', ' ', ' '])
       game.instance_variable_set(:@grid, winning_grid)
